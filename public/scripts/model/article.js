e5489a5d379b915264f3e5a7b906edc19e60530f
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-06-20 15:25:36 -0700

    reduce and filter Article.all

--- public/scripts/model/article.js
+++ public/scripts/model/article.js
@@ -12,8 +12,8 @@ var app = app || {};
             .then(data => {
 				sourceArticles.all = (JSON.parse(data).articles);
 				sourceArticles.all.forEach(obj => obj.source = JSON.parse(data).source);
+				sourceArticles.all.forEach(obj => obj.shown = false);
 			}, err => console.error(err))
-            // .then(data => console.log(data), err => console.error(err))
             .then(callback);
 	};
 
@@ -21,17 +21,25 @@ var app = app || {};
     // COMMENT: stretch goal: filtering by source or something else?
     // sourceArticles.with = attr => sourceArticles.all.filter( sourceArticle => sourceArticle[attr]);
     sourceArticles.requestArticles(loadArticles);
-		
-
-		function Article (sourceArticleData) {
-			Object.keys(sourceArticleData).forEach(key => this[key] = sourceArticleData[key]);
-		}
 
 
-		function loadArticles() {
-			Article.all = sourceArticles.all.map(obj => new Article(obj));
-			console.log('sourceArticles.all is', sourceArticles.all, 'Article.all is', Article.all);
-		}
+	function Article(sourceArticleData) {
+		Object.keys(sourceArticleData).forEach(key => this[key] = sourceArticleData[key]);
+	}
+
+	Article.all = [];
+
+	function loadArticles() {
+		Article.all = sourceArticles.all.map(obj => new Article(obj))
+		.reduce((titles, title) => {
+			if (titles.indexOf(title) === -1) titles.push(title);
+			return titles;
+		}, [])
+		.filter(t => t.shown === false);
+
+		console.log('Article.all is', Article.all);
+	}
+
 
 	module.sourceArticles = sourceArticles;
 
