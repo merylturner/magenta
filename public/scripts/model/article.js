ed38db45934ca1888427de607f6c15598b2b34f7
Pierre Salumu-Shabani <jrsalshab@outlook.com>
2017-06-20 12:49:05 -0700

    Source article data

--- public/scripts/model/article.js
+++ public/scripts/model/article.js
@@ -5,14 +5,13 @@ var app = app || {};
 (function (module) {
 
 	const sourceArticles = {};
-	sourceArticles.all = [];
-    console.log(sourceArticles.all);
+	// sourceArticles.all = [];
 
 	sourceArticles.requestArticles = function (callback) {
 		console.log('requestArticles is listening');
         $.get('/news')
-            // .then(data => sourceArticles.all = data, err => console.error(err))
-            .then(data => console.log(data), err => console.error(err))
+            .then(data => console.log(JSON.parse(data).articles), err => console.error(err))
+            // .then(data => console.log(data), err => console.error(err))
             .then(callback);
 	};
 
@@ -20,6 +19,15 @@ var app = app || {};
     // COMMENT: stretch goal: filtering by source or something else?
     // sourceArticles.with = attr => sourceArticles.all.filter( sourceArticle => sourceArticle[attr]);
     sourceArticles.requestArticles();
+		function Article (sourceArticleData) {
+			Object.keys(sourceArticleData.articles).forEach(key => this[key] = sourceArticleData[key]);
+			console.log(sourceArticleData.articles);
+		}
+		Article.all = [];
+		// Article.all = sourceArticles.all.map(obj => new Article (obj));
+		console.log(Article.all);
+
+
 	module.sourceArticles = sourceArticles;
 
 }(app));
