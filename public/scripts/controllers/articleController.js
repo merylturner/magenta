12e5a4458f6997f48de2e8fd17880e420421dcd6
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-06-20 18:22:57 -0700

    headline showing on index

--- public/scripts/controllers/articleController.js
+++ public/scripts/controllers/articleController.js
@@ -5,6 +5,14 @@ var app = app || {};
 (function(module) {
 	const articleController = {};
 
+	articleController.randomArticle = function() {
+		return app.Article.selectRandom(app.Article.loadArticles());
+	};
+	
+	// COMMENT: stretch goal: filtering by source or something else?
+	// sourceArticles.with = attr => sourceArticles.all.filter( sourceArticle => sourceArticle[attr]);
+	app.sourceArticles.requestArticles(app.articleView.init);
+
 
 	module.articleController = articleController;
 }(app));
--- public/scripts/model/article.js
+++ public/scripts/model/article.js
@@ -8,20 +8,15 @@ var app = app || {};
 	sourceArticles.all = [];
 
 	sourceArticles.requestArticles = function (callback) {
-        $.get('/news')
-            .then(data => {
+		$.get('/news')
+			.then(data => {
 				sourceArticles.all = (JSON.parse(data).articles);
 				sourceArticles.all.forEach(obj => obj.source = JSON.parse(data).source);
 				sourceArticles.all.forEach(obj => obj.shown = false);
 			}, err => console.error(err))
-            .then(callback);
+			.then(Article.loadArticles)
+			.then(callback);
 	};
-
-
-    // COMMENT: stretch goal: filtering by source or something else?
-    // sourceArticles.with = attr => sourceArticles.all.filter( sourceArticle => sourceArticle[attr]);
-    sourceArticles.requestArticles(loadArticles);
-
 
 	function Article(sourceArticleData) {
 		Object.keys(sourceArticleData).forEach(key => this[key] = sourceArticleData[key]);
@@ -29,18 +24,22 @@ var app = app || {};
 
 	Article.all = [];
 
-	function loadArticles() {
+	Article.loadArticles = function() {
 		Article.all = sourceArticles.all.map(obj => new Article(obj))
-		.reduce((titles, title) => {
-			if (titles.indexOf(title) === -1) titles.push(title);
-			return titles;
-		}, [])
-		.filter(t => t.shown === false);
+			.reduce((titles, title) => {
+				if (titles.indexOf(title) === -1) titles.push(title);
+				return titles;
+			}, [])
+			.filter(t => t.shown === false);
 
-		console.log('Article.all is', Article.all);
-	}
+		return Article.all;
+	};
 
+	Article.selectRandom = function (array) {
+		let randomNum = Math.floor(Math.random() * (array.length));
+		return array[randomNum];
+	};
 
 	module.sourceArticles = sourceArticles;
-
+	module.Article = Article;
 }(app));
