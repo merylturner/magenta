4b39a6c6117f07358cbd45e6b40ff6fde2d835cd
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-06-23 11:03:12 -0700

    Reformat text of vote and source display

--- public/scripts/controllers/resultsController.js
+++ public/scripts/controllers/resultsController.js
@@ -13,7 +13,23 @@ var app = app || {};
         app.selectedObj.voteCenter = resultsObj.count_center;
         app.selectedObj.voteCenterRight = resultsObj.count_center_right;
         app.selectedObj.voteRight = resultsObj.count_right;
-        console.log(app.selectedObj);
+
+        app.selectedObj.selectionToDisplay = app.selectedObj.selection;
+        app.selectedObj.sourceToDisplay = app.selectedObj.source;
+
+        if (app.selectedObj.selectionToDisplay === 'voteLeft') app.selectedObj.selectionToDisplay = 'Left'
+        if (app.selectedObj.selectionToDisplay === 'voteCenterLeft') app.selectedObj.selectionToDisplay = 'Center Left'
+        if (app.selectedObj.selectionToDisplay === 'voteCenter') app.selectedObj.selectionToDisplay = 'Center'
+        if (app.selectedObj.selectionToDisplay === 'voteCenterRight') app.selectedObj.selectionToDisplay = 'Center Right'
+        if (app.selectedObj.selectionToDisplay === 'voteRight') app.selectedObj.selectionToDisplay = 'Right'
+
+        if (app.selectedObj.source === 'the-new-york-times') app.selectedObj.sourceToDisplay = 'The New York Times'
+        if (app.selectedObj.source === 'the-huffington-post') app.selectedObj.sourceToDisplay = 'The Huffington Post'
+        if (app.selectedObj.source === 'daily-mail') app.selectedObj.sourceToDisplay = 'Daily Mail'
+        if (app.selectedObj.source === 'usa-today') app.selectedObj.sourceToDisplay = 'USA Today'
+        if (app.selectedObj.source === 'breitbart-news') app.selectedObj.sourceToDisplay = 'Breitbart News'
+        
+
         app.resultsView.init();
         return resultsObj;
 
